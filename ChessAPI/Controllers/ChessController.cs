b899e6ee03f250e644d359a370e246c258dd804e
Paweł <pawelgawron2002@gmail.com>
2025-04-12 18:40:25 +0200

    Implement controller logic, add url to public API, create a http request and handle it

--- ChessAPI/Controllers/ChessController.cs
+++ ChessAPI/Controllers/ChessController.cs
@@ -1,9 +1,43 @@
-﻿using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 
 namespace ChessAPI.Controllers;
+
 [Route("api/[controller]")]
 [ApiController]
 public class ChessController : ControllerBase
 {
+    private readonly HttpClient _httpClient;
+
+    public ChessController(HttpClient httpClient)
+    {
+        _httpClient = httpClient;
+    }
+
+    [HttpGet("{username}")]
+    public async Task<IActionResult> GetUserInfo(string username)
+    {
+        var url = $"https://api.chess.com/pub/player/{username}";
+
+        // Logowanie URL w celu sprawdzenia poprawności URL
+        Console.WriteLine($"Requesting Chess.com API: {url}");
+
+        // Tworzymy zapytanie HTTP
+        var requestMessage = new HttpRequestMessage(HttpMethod.Get, url);
+        requestMessage.Headers.Add("Accept", "application/json");
+        requestMessage.Headers.Add("User-Agent", "ChessInfoAPI/1.0 (http://localhost:7068)"); // Wymagana identyfikacja przez API chess.com
+
+        var response = await _httpClient.GetAsync(url);
+
+        if (response.IsSuccessStatusCode)
+        {
+            var userInfo = await response.Content.ReadAsStringAsync();
+            Console.WriteLine($"Response from Chess.com: {userInfo}"); //Logowanie odpowiedzi
+            return Ok(userInfo);
+        }
+        else
+        {
+            Console.WriteLine($"Error: {response.StatusCode} - {response.ReasonPhrase}"); // Logowanie błędu
+            return NotFound(new { Message = $"User {username} not found!" });
+        }
+    }
 }
