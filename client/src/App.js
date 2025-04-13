1132514c8b08acd59202897453566c30484c52fb
Paweł <pawelgawron2002@gmail.com>
2025-04-13 12:07:32 +0200

    Added React client and reorganized backend project structure

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ChessUserInfo from "./components/ChessUserInfo";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Welcome to Chess Info App v1.0</h1>
+      <ChessUserInfo />
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/components/ChessUserInfo.js
@@ -0,0 +1,60 @@
+import React, { useState } from "react";
+import axios from "axios";
+
+const ChessUserInfo = () => {
+  const [username, setUsername] = useState("");
+  const [userInfo, setUserInfo] = useState(null);
+  const [error, setError] = useState("");
+
+  // Funkcja powinna być zdefiniowana wewnątrz komponentu
+  const fetchUserInfo = async () => {
+    try {
+      const response = await axios.get(
+        `https://localhost:7281/api/Chess/${username}`
+      );
+      console.log(response);
+      setUserInfo(response.data);
+      setError("");
+    } catch (ex) {
+      console.error(ex);
+      setError("User not found or error occurred!");
+      setUserInfo(null);
+    }
+  };
+
+  return (
+    <div>
+      <h1>Chess User Information</h1>
+      <input
+        type="text"
+        value={username}
+        onChange={(e) => setUsername(e.target.value)}
+        placeholder="Enter chess.com username"
+      />
+      <button onClick={fetchUserInfo}>Get info</button>
+
+      {error && <p style={{ color: "red" }}>{error}</p>}
+
+      {userInfo && (
+        <div>
+          <h2>User Info:</h2>
+          <p>
+            <strong>Username:</strong> {userInfo.username}
+          </p>
+          <p>
+            <strong>Name:</strong> {userInfo.name}
+          </p>
+          <p>
+            <strong>Country:</strong> {userInfo.country}
+          </p>
+          <p>
+            <strong>Followers:</strong> {userInfo.followers}
+          </p>
+          <img src={userInfo.avatar} alt={userInfo.username} width="100" />
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default ChessUserInfo;
