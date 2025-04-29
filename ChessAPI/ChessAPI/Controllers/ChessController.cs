aa46e1527951297132c76ae2b5e85687637f1833
pkrzysiek <pkrzysiek24@gmail.com>
2025-04-29 18:45:56 +0200

    Update controller and add unix time converter

--- ChessAPI/ChessAPI/Controllers/ChessController.cs
+++ ChessAPI/ChessAPI/Controllers/ChessController.cs
@@ -22,39 +22,6 @@ public class ChessController : ControllerBase
     {
         var userInfo = await _chessService.GetUserInfo(username);
 
-        if (userInfo == null)
-        {
-            return NotFound(new { Message = $"User {username} not found!" });
-        }
-
-        List<ChessUser> usersList = new List<ChessUser>();
-
-        if (System.IO.File.Exists(filePath))
-        {
-            try
-            {
-                var jsonString = await System.IO.File.ReadAllTextAsync(filePath);
-
-                if (!string.IsNullOrWhiteSpace(jsonString))
-                {
-                    usersList = JsonSerializer.Deserialize<List<ChessUser>>(jsonString);
-                }
-            }
-            catch (JsonException ex)
-            {
-                Console.WriteLine($"Error: {ex.Message}");
-            }
-            catch(Exception ex)
-            {
-                Console.WriteLine($"Error: {ex.Message}");  
-            }
-        }
-
-        usersList.Add(userInfo);
-
-        var updatedJson = JsonSerializer.Serialize(usersList, new JsonSerializerOptions { WriteIndented = true });
-        await System.IO.File.WriteAllTextAsync(filePath, updatedJson);
-
         return Ok(userInfo);
     }
 }
