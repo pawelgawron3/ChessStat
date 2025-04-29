57fe08c2e857e0a3325045babc7937b356f6c671
pkrzysiek <pkrzysiek24@gmail.com>
2025-04-29 18:45:39 +0200

    Add models to help deserialize the data

---
+++ ChessAPI/ChessAPI/Models/UserCountry.cs
@@ -0,0 +1,9 @@
+﻿using System.Text.Json.Serialization;
+
+namespace ChessAPI.Models;
+
+public class UserCountry
+{
+    [JsonPropertyName("name")]
+    public string Name { get; set; } = string.Empty;
+}
