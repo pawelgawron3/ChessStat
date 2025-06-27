8682f4a5efa75b9884f8ec15a39c78959ee93661
pawelgawron3 <148467604+pawelgawron3@users.noreply.github.com>
2025-06-27 19:12:19 +0200

    Change the layout title

--- client/src/App.js
+++ client/src/App.js
@@ -2,10 +2,18 @@ import React from "react";
 import UserInfo from "./components/UserInfo/UserInfo";
 import "./App.css";
 
+function ChessFigures() {
+  return <span className="title-figures">&#9813;&#9812;</span>;
+}
+
 function App() {
   return (
     <div className="App">
-      <h1 className="app-header">Welcome to Chess Info App</h1>
+      <h1 className="app-header">
+        <ChessFigures />
+        Welcome to Chess Info App
+        <ChessFigures />
+      </h1>
       <UserInfo />
     </div>
   );
